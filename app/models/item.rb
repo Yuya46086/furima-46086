d21cbb37e7705159f5acd6a9615c97aacad0d025
Yuya46086 <fuji40726@gmail.com>
2025-09-10 20:26:10 +0900

    itemsテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+end

d981904bad7e2d6e7e1d3b64de0f591d7726dda6
Yuya46086 <fuji40726@gmail.com>
2025-09-12 23:15:08 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
   belongs_to :user
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_fee
-  belongs_to :prefecture
-  belongs_to :shipping_date
   has_one_attached :image
+
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end

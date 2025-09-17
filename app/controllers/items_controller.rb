de75c86ac7e2762f8546f7236aa8079baefc6332
Yuya46086 <fuji40726@gmail.com>
2025-09-18 00:41:31 +0900

    destroyアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -32,6 +32,16 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+      return
+    end
+
+    @item.destroy
+    redirect_to root_path
   end
 
   private

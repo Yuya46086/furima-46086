f39a600a9da9f5c91299a46aee7e128498722547
Yuya46086 <fuji40726@gmail.com>
2025-09-18 18:14:38 +0900

    コントローラー内の記述の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
@@ -35,11 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-      return
-    end
-
     @item.destroy
     redirect_to root_path
   end
@@ -48,6 +43,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def check_user
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def item_params

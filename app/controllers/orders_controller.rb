8432167578dd9f55163e86998c0529f722961d5b
Yuya46086 <fuji40726@gmail.com>
2025-09-21 20:34:39 +0900

    ordersコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:new]
+
+  def new
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end

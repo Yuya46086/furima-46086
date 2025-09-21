7acad8d120c17c7ecc92b617121c6478ca7a38f8
Yuya46086 <fuji40726@gmail.com>
2025-09-21 23:18:21 +0900

    Revert "購入情報保存のための実装"
    This reverts commit fff3188c000ca53338da4d74b25ff3d067d9f38f.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,22 +6,11 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
-    redirect_to root_path
   end
 
   private
 
   def set_item
     @item = Item.find(params[:item_id])
-  end
-
-  def order_params
-    params.merge(user_id: current_user.id, item_id: @item.id)
-  end
-
-  def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :address_line, :building, :phone_number).merge(order_id: @order.id)
   end
 end

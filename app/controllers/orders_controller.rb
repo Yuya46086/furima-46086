dd5ff4834e1d40de1351101ee43c65b268ed8f0e
Yuya46086 <fuji40726@gmail.com>
2025-09-24 23:40:07 +0900

    「決済機能のサーバーサイド実装」ならびに「環境変数の設定」

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,31 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:new]
+  before_action :set_item, only: [:new, :create]
 
   def new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building,
+                                          :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 end

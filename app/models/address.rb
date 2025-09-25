308cba778695f7389c827b0b06ee0d970f762ad8
Yuya46086 <fuji40726@gmail.com>
2025-09-25 20:25:28 +0900

    Formオブジェクトパターンでの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,14 +1,5 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order
   belongs_to_active_hash :prefecture
-
-  with_options presence: true do
-    validates :postal_code
-    validates :city
-    validates :address_line
-    validates :phone_number
-  end
-
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end

6afb4f7b30f0b226c9b1a0c0f91c8a036f62a636
Yuya46086 <fuji40726@gmail.com>
2025-09-25 20:21:58 +0900

    売却済みの場合の条件処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:new, :create]
+  before_action :redirect_if_condition_met, only: [:new, :create]
 
   def new
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
@@ -27,5 +28,10 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building,
                                           :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def redirect_if_condition_met
+    redirect_to root_path unless @item.user_id != current_user.id
+    redirect_to root_path if @item.order
   end
 end
